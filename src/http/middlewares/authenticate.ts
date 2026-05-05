d02d300159b9ecae2811394d58e0b07b8d0eee70
Pedro lenin <pedrolenin06@gmail.com>
2026-05-05 16:51:21 -0300

    fix(authenticate.ts): use FastifyJWT interface augmentation to type req.user correctly

--- src/http/middlewares/authenticate.ts
+++ src/http/middlewares/authenticate.ts
@@ -1,8 +1,8 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import type { IContractAuthProvider } from "../../infra/auth/contractAuthProvider";
 
-declare module "fastify" {
-  interface FastifyRequest {
+declare module "@fastify/jwt" {
+  interface FastifyJWT{
     user: {
       id: string;
       email: string | undefined;
